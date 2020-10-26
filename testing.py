7665c3301a5a5844521191158f6fde5ac617a8e6
Mateleo <mateleo78@gmail.com>
2020-10-26 22:03:49 +0100

    test pour shampooing

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ file = open("bible/BiblePlain.txt", "r", encoding="UTF-8")
 bible = file.read()
 
 letterList = ['e','o','t','u','r','a','n']
+
 accentE = ['é','è','ê']
 validList = letterList.append('\n')
 
@@ -14,5 +15,5 @@ def accentRemover(char):
         return char
 
 for letter in range(0,len(bible)):
-    if(letter=="\n" or letter==" "):
-        print()
+    if(ord(bible[letter])>=65 and ord(bible[letter])<=90):
+        print(bible[letter])
