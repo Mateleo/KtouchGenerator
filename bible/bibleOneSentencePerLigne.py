cc58b2ed0f9392847cb7787c6d58cd0601bc209e
Mateleo <mateleo78@gmail.com>
2020-10-28 15:26:01 +0100

    bible transformation almost finish.

--- bible/bibleOneSentencePerLigne.py
+++ bible/bibleOneSentencePerLigne.py
@@ -4,12 +4,9 @@ fileToRead.close()
 
 fileToWrite = open("BiblePhraseLignes.txt", "w", encoding="UTF-8")
 
-
-
 for letter in range(0,len(bible)):
-var = ord(bible[letter])
-
-    if (var <= 65 or var >=90) or (ord(bible[letter+1]) <= 65 or ord(bible[letter+1]) >=90)
-        if(var<48 or var>57) and (var<127 or var>192)):
+    var = ord(bible[letter])
+    if(var <= 65 or var >=90 or ord(bible[letter+1])<= 65 or ord(bible[letter+1]) >=90):
+        if((var<48 or var>57) and (var<127 or var>192)):
             if (var < 256 and var != 10):
                 fileToWrite.write(bible[letter])
--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
 file = open("bible/BiblePlain.txt", "r", encoding="UTF-8")
 bible = file.read()
 
+fileToWrite = open("BibleLignes.txt", "w", encoding="UTF-8")
+
 letterList = ['e','o','t','u','r','a','n']
 
 accentE = ['é','è','ê']
@@ -15,5 +17,14 @@ def accentRemover(char):
         return char
 
 for letter in range(0,len(bible)):
-    if(ord(bible[letter])>=65 and ord(bible[letter]<=90):
-        print(bible[letter+1])
+    if((ord(bible[letter])==32) and (ord(bible[letter-1])!=46)): #si espace et avant point ne pas écrire
+        fileToWrite.write(bible[letter])
+    elif(bible[letter]=="\n"): #si retour à la ligne mettre un espace
+        fileToWrite.write(' ')
+    elif((ord(bible[letter])>=65) and (ord(bible[letter])<=90)): #si majuscule
+        if(ord(bible[letter-2])==46 or ord(bible[letter-4])==46): #si point avant faire un retour à la ligne
+            fileToWrite.write("\n")
+        fileToWrite.write(bible[letter])
+    elif((ord(bible[letter])>=97 and ord(bible[letter])<=122) or (ord(bible[letter])==39) or (ord(bible[letter])==46) or (ord(bible[letter])>=224 and ord(bible[letter])<=255)): #lettre et tout le merdier
+        fileToWrite.write(bible[letter])
+fileToWrite.close()
